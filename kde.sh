a491e1ebb0990dda3a6f97dd2dc57369b93e5387
mrkkrp <markkarpov92@gmail.com>
2019-05-27 22:18:06 +0200

    Update the ‘kde.sh’ just a little bit

--- kde.sh
+++ kde.sh
@@ -5,8 +5,12 @@
 set -e
 
 echo "This is not automated (yet):"
-echo "1. Set keyboard shortcuts."
+echo "1. Set keyboard shortcuts:"
+echo "   Alt+4 -> konsole --fullscreen"
+echo "   Alt+2 -> google-chrome-stable"
+echo "   Alt+3 -> emacs"
+echo "   Alt+1 -> dolphin"
 echo "2. Enable sticky keys."
-echo "3. Speed of key repeat."
+echo "3. Speed of key repeat (delay=600, speed=50 per second)."
 echo "4. Dolphin should show hidden files."
 echo "5. Enable compose key (keyboard -> advanced -> position of compose key)."
