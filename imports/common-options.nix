e4a11e007176ceefd73374c990499376137b31f7
Mark Karpov <markkarpov92@gmail.com>
2020-03-29 20:32:56 +0200

    Persist Konsole setup

--- imports/common-options.nix
+++ imports/common-options.nix
@@ -171,6 +171,7 @@
     symlinks = {
       ".config/kglobalshortcutsrc" = ./kglobalshortcutsrc;
       ".config/khotkeysrc" = ./khotkeysrc;
+      ".config/konsolerc" = ./konsolerc;
       ".emacs.d/init.el" = ./emacs/init.el;
       ".emacs.d/snippets" = ./emacs/snippets;
       ".gitconfig" = pkgs.gitconfig;
