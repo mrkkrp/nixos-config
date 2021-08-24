8e5b4cefae5939c6df5abb4d2f5b9930e2fb7c9f
Mark Karpov <markkarpov92@gmail.com>
2021-08-24 22:55:38 +0200

    Add config for the new ThinkPad

---
+++ devices/pad/configuration.nix
@@ -0,0 +1,43 @@
+{ config, pkgs, ... }:
+
+let
+  nixPkgsRepo = "/home/mark/nixpkgs";
+  nixConfigRepo = "/home/mark/projects/mrkkrp/nixos-config";
+in rec {
+
+  imports = [
+    ./hardware-configuration.nix
+    ./../../imports/common-options.nix
+    ./../../imports/nginx.nix
+    ./../../imports/pulseaudio.nix
+    # ./../../imports/jack.nix
+    ];
+
+  networking.hostName = "pad";
+  time.timeZone = "Europe/Paris";
+
+  location = {
+    latitude = 48.864716;
+    longitude = 2.349014;
+  };
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  nix = {
+    package = pkgs.nixUnstable;
+    binaryCaches = [
+      "https://cache.nixos.org"
+    ];
+    gc.automatic = false;
+    nixPath = [
+      "nixpkgs=${nixPkgsRepo}"
+      "nixos-config=${nixConfigRepo}/devices/${networking.hostName}/configuration.nix"
+    ];
+  };
+
+  hardware.pulseaudio = {
+    enable = true;
+    package = pkgs.pulseaudioFull;
+  };
+}
