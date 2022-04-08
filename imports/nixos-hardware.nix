a95dcd5e712621d94d5f55e2a2065104c74b3eb8
Mark Karpov <markkarpov92@gmail.com>
2022-04-08 13:48:11 +0200

    Remove a redundant file (we source nixos-hardware as a flake now)

--- imports/nixos-hardware.nix
+++
@@ -1,4 +0,0 @@
-builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixos-hardware/archive/5a7e613703ea349fd46b3fa2f3dfe3bd5444d591.tar.gz";
-  sha256 = "sha256:088z9p9ycsvnghqbksxrssk43wfsnm9caks9lch90jp2x8c8aw7x";
-}
