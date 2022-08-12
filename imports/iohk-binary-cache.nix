043fba0ca1d245b0a0b4d8195ff60e43d5d7d2ca
Mark Karpov <markkarpov92@gmail.com>
2022-08-12 15:28:45 +0200

    Update the IOHK's binary cache substituter

--- imports/iohk-binary-cache.nix
+++ imports/iohk-binary-cache.nix
@@ -4,7 +4,7 @@
   nix.settings = {
     substituters = [
       "https://cache.nixos.org"
-      "https://hydra.iohk.io"
+      "https://cache.iog.io"
     ];
     trusted-public-keys = [
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
