27528de2e2ea22e2c8a1387be0f4a0c6e0c9cb66
Mark Karpov <markkarpov92@gmail.com>
2025-11-17 23:25:23 +0100

    Emacs: set tramp-use-connection-share to nil
    Otherwise Magit seems to have a hard time over ssh.

--- imports/emacs/mk-packages.el
+++ imports/emacs/mk-packages.el
@@ -1129,9 +1129,7 @@ input method."
 
 (use-package tramp-sh
   :init
-  (setq
-   tramp-ssh-controlmaster-options
-   "-o ControlMaster=auto -o ControlPath=tramp.%%C -o ControlPersist=600"))
+  (setq tramp-use-connection-share nil))
 
 (use-package visual-regexp
   :bind
