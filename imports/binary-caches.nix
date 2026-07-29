18c6a543a3386dd98003343d60a4ae243d67b149
Mark Karpov <markkarpov92@gmail.com>
2026-07-29 07:53:23 +0200

    Source binary caches for my sites

--- imports/binary-caches.nix
+++ imports/binary-caches.nix
@@ -10,6 +10,7 @@
     trusted-public-keys = [
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "ormolu.cachix.org-1:0L9Y4A+6dGpvfGtaeaq5w44pgX0AVRivKMfi2fiOzYE="
+      "markkarpov-sites.cachix.org-1:tzrAG4NHl/VkbtjotbuQJ7kCSaq/dkzj2IaSUgxo4Gs="
     ];
   };
 }
