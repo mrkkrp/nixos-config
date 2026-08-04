db3cffadcce3fcdf079f09ecd225063ad019cbf0
Mark Karpov <markkarpov92@gmail.com>
2026-08-04 22:09:13 +0200

    Adjust the font size in wezterm (46.0 -> 32.0)

--- imports/.config/wezterm.lua
+++ imports/.config/wezterm.lua
@@ -35,7 +35,7 @@ config.keys = {
 
 config.color_scheme = 'Zenburn'
 config.font = wezterm.font('DejaVu Sans Mono')
-config.font_size = 46.0
+config.font_size = 32.0
 config.front_end = 'WebGpu'
 config.audible_bell = 'Disabled'
 config.window_padding = {
